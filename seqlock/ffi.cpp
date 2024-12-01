f09293e1f0ba0a4233b3f7ef312ad5cc9aee31f9
sergiu128 <sergiu@tungsten.computer>
2024-12-01 22:07:43 +0100

    Rework implementation of ffi.cpp

--- seqlock/ffi.cpp
+++ seqlock/ffi.cpp
@@ -13,56 +13,45 @@ extern "C" {
 #endif
 
 struct SingleWriterSeqLock* seqlock_single_writer_create(char* data, size_t size) {
-    struct SingleWriterSeqLock* wrapper_lock{nullptr};
-    try {
-        wrapper_lock = new struct SingleWriterSeqLock;
-        wrapper_lock->lock = new seqlock::SeqLock<seqlock::mode::SingleWriter>;
-        wrapper_lock->shm = nullptr;
-        wrapper_lock->shared_data = data;
-        wrapper_lock->shared_data_size = size;
-        wrapper_lock->shared = false;
-    } catch (const std::exception& e) {
-        std::cerr << "seqlock FFI error in C++: " << __FILE_NAME__ << " err: " << e.what() << std::endl;
-        errno = EIO;
-    } catch (...) {
-        std::cerr << "unknown seqlock FFI error in C++: " << __FILE_NAME__ << std::endl;
-        errno = EIO;
-    }
+    // TODO still catch exceptions
+    auto* wrapper_lock = new struct SingleWriterSeqLock;
+    wrapper_lock->lock = new seqlock::SeqLock<seqlock::mode::SingleWriter>;
+    wrapper_lock->shm = nullptr;
+    wrapper_lock->shared_data = data;
+    wrapper_lock->shared_data_size = size;
+    wrapper_lock->shared = false;
 
     return wrapper_lock;
 }
 
 struct SingleWriterSeqLock* seqlock_single_writer_create_shared(const char* filename, size_t size) {
-    struct SingleWriterSeqLock* wrapper_lock{nullptr};
-    try {
-        wrapper_lock = new struct SingleWriterSeqLock;
+    using T = seqlock::SeqLock<seqlock::mode::SingleWriter>;
 
-        const size_t seqlock_size = sizeof(seqlock::SeqLock<seqlock::mode::SingleWriter>);
-        size += seqlock_size;
-        auto* shm = new seqlock::util::SharedMemory{
-            filename, size, [](const std::exception& e) { std::cerr << e.what() << std::endl; }};
-        auto* lock = shm->Map<seqlock::SeqLock<seqlock::mode::SingleWriter>>();
+    const size_t seqlock_size = sizeof(T);
+    size += seqlock_size;
 
-        wrapper_lock->lock = lock;
-        wrapper_lock->shm = shm;
-        wrapper_lock->shared_data = (char*)shm->Ptr() + seqlock_size;
-        assert(shm->Size() > seqlock_size);
-        wrapper_lock->shared_data_size = shm->Size() - seqlock_size;
-        wrapper_lock->shared = true;
-    } catch (const std::exception& e) {
-        std::cerr << "seqlock FFI error in C++: " << __FILE_NAME__ << " err: " << e.what() << std::endl;
-        errno = EIO;
-    } catch (...) {
-        std::cerr << "unknown seqlock FFI error in C++: " << __FILE_NAME__ << std::endl;
-        errno = EIO;
+    auto shm_result = seqlock::util::SharedMemory<T>::Factory(std::string{filename}, size);
+    if (not shm_result) {
+        std::cerr << shm_result.error() << std::endl;
+        return nullptr;
     }
+    auto* shm = new seqlock::util::SharedMemory<T>(std::move(shm_result.value()));
+
+    auto* wrapper_lock = new struct SingleWriterSeqLock;
+    wrapper_lock->lock = shm->GetRaw();
+    wrapper_lock->shm = static_cast<void*>(shm);
+    wrapper_lock->shared_data = static_cast<char*>(shm->GetRaw()) + seqlock_size;
+    wrapper_lock->shared_data_size = shm->Size() - seqlock_size;
+    wrapper_lock->shared = true;
 
     return wrapper_lock;
 }
 
 void seqlock_single_writer_destroy(struct SingleWriterSeqLock* wrapper_lock) {
+    using T = seqlock::SeqLock<seqlock::mode::SingleWriter>;
+
     if (wrapper_lock->shared) {
-        delete (seqlock::util::SharedMemory*)(wrapper_lock->shm);  // noexcept
+        delete (seqlock::util::SharedMemory<T>*)(wrapper_lock->shm);  // noexcept
     } else {
         delete (seqlock::SeqLock<seqlock::mode::SingleWriter>*)(wrapper_lock->lock);  // noexcept
     }
